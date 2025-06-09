6e7f308983a59e9f10eab3cfe62e9d417c822b29
tanmaykulkarni2112 <tanmaykulkarni2112@gmail.com>
2025-06-10 00:55:12 +0530

    Set up frontend dependencies

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes , Route} from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import {Toaster, toast} from 'react-hot-toast';
+
+const App = () => {
+  return (
+    <div data-theme="forest">
+      <button className='btn btn-primary'>Click me</button>
+      <Routes>
+        <Route path = '/' element = {<HomePage/>} />
+        <Route path ='/create' element = {<CreatePage/>} />
+        <Route path ='/note/:id' element = {<NoteDetailPage/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
